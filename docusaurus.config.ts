94067c2c86eb6bb9ad99c473ea6ac98af9fc2c16
Péter Kardos <kardospeter1994@hotmail.com>
2025-03-27 18:09:46 +0100

    gh pages (#1)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,16 +9,10 @@ const config: Config = {
   tagline: 'Manage your self-encrypting drives',
   favicon: 'img/logo.ico',
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'petiaccja', // Usually your GitHub org/user name.
-  projectName: 'sed-manager-rs', // Usually your repo name.
+  url: 'https://petiaccja.github.io',
+  baseUrl: '/sed-manager-website',
+  organizationName: 'petiaccja',
+  projectName: 'sed-manager-website',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
