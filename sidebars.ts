fa5e8c7879844687ddb482e51e342e2c223f3d55
Péter Kardos <kardospeter1994@hotmail.com>
2025-03-29 23:11:44 +0100

    added more docs (#2)

--- sidebars.ts
+++ sidebars.ts
@@ -4,8 +4,8 @@ import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
 
 const sidebars: SidebarsConfig = {
   introToSEDs: [
-    'sed/fde',
-    'sed/standards',
+    'background/seds',
+    'background/tcg_specs',
   ],
   usingSEDManager: [
     'usage/introduction',
