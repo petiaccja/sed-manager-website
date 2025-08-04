69e2be6d7bb2cdb31e88dccbe4d65a55b1791563
Péter Kardos <kardospeter1994@hotmail.com>
2025-08-04 13:22:37 +0200

    Improvements (#5)
    * redesign download page, add slint attribution badge
    
    * add stuff to footer

--- src/pages/downloads/index.tsx
+++ src/pages/downloads/index.tsx
@@ -1,64 +1,97 @@
 import styles from './downloads.module.css';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
-import DownloadIcon from '@site/static/img/ui/download.svg';
-import BalanceIcon from '@site/static/img/ui/balance.svg';
+import ThemedImage from '@theme/ThemedImage';
+import Tabs from '@theme/Tabs'
+import TabItem from '@theme/TabItem'
 
 export default function Downloads() {
     return (
         <Layout title={`Downloads`} description="Download precompiled executables and PBA images">
-            <main style={{ maxWidth: '960px', margin: '0 auto' }}>
-                <h1>
-                    Latest release
-                </h1>
+            <main style={{ maxWidth: '996px', margin: '0 auto', display: "flex", flexDirection: "column" }}>
+                <Tabs
+                    groupId="operating-system"
+                    defaultValue="windows"
+                    values={[
+                        { label: 'Windows', value: 'windows' },
+                        { label: 'Linux', value: 'linux' }
+                    ]}
+                    block={true}
+                >
+                    <TabItem value="windows">
+                        <table className={styles.downloadTable}>
+                            <tbody>
+                                <tr>
+                                    <td>x86-64</td>
+                                    <td>
+                                        <Link to="https://github.com/petiaccja/sed-manager-rs/releases/latest/download/SEDManager_Windows_x86_64.zip">
+                                            SEDManager_Windows_x86_64.zip
+                                        </Link>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </TabItem>
+                    <TabItem value="linux">
+                        <table className={styles.downloadTable}>
+                            <tbody>
+                                <tr>
+                                    <td>x86-64</td>
+                                    <td>
+                                        <Link to="https://github.com/petiaccja/sed-manager-rs/releases/latest/download/SEDManager_Linux_x86_64.zip">
+                                            SEDManager_Linux_x86_64.zip
+                                        </Link>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </TabItem>
+                </Tabs>
 
-                <h2>Application</h2>
+                <div style={{ minHeight: "32px" }}></div>
 
-                <table className={styles.downloadTable}>
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Windows</th>
-                            <th>Linux</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>x86-64</td>
-                            <td>
-                                <Link to="https://github.com/petiaccja/sed-manager-rs/releases/latest/download/SEDManager_Windows_x86_64.zip">
-                                    SEDManager_Windows_x86_64.zip
-                                </Link>
-                            </td>
-                            <td>
-                                <Link to="https://github.com/petiaccja/sed-manager-rs/releases/latest/download/SEDManager_Linux_x86_64.zip">
-                                    SEDManager_Linux_x86_64.zip
-                                </Link>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <Tabs
+                    groupId="pba"
+                    defaultValue="pba"
+                    values={[
+                        { label: 'Pre-boot authentication', value: 'pba' },
+                    ]}
+                    block={true}
+                >
+                    <TabItem value='pba'>
+                        <table className={styles.downloadTable}>
+                            <tbody>
+                                <tr>
+                                    <td>x86-64</td>
+                                    <td>
+                                        <Link to="https://github.com/petiaccja/sed-manager-pba/releases/latest/download/SEDManagerPBA_x86_64.zip">
+                                            SEDManagerPBA_x86_64.zip
+                                        </Link>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </TabItem>
+                </Tabs>
 
-                <h2>Pre-boot authentication</h2>
+                <div style={{ flex: 100, minHeight: "48px" }}></div>
 
-                <table className={styles.downloadTable}>
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th>Bootable ISO</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>x86-64</td>
-                            <td>
-                                <Link to="https://github.com/petiaccja/sed-manager-pba/releases/latest/download/SEDManagerPBA_x86_64.zip">
-                                    SEDManagerPBA_x86_64.zip
-                                </Link>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+                <div>
+                    <center>
+                        <Link to="https://slint.dev">
+                            <ThemedImage
+                                alt="Made with slint"
+                                width={"168px"}
+                                sources={{
+                                    light: "https://slint.dev/logo/MadeWithSlint-logo-light.svg",
+                                    dark: "https://slint.dev/logo/MadeWithSlint-logo-dark.svg",
+                                }}
+                            />
+                        </Link>
+                        <br />
+                        SEDManager was built using Slint, a powerful UI toolkit.
+                    </center>
+                </div>
             </main>
         </Layout>
     );
